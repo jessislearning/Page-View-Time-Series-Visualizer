5475ecbbefc48cd7bcba88341f23919bb69b3b34
jessislearning a <jessica.afalla.29@gmail.com>
2024-03-27 12:04:47 +0000

    step 1 lineplot, need to work on x label ticks

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 # This entrypoint file to be used in development. Start by reading README.md
-import time_series_visualizer
-from unittest import main
+#import time_series_visualizer
+#from unittest import main
 
 # Test your function by calling it here
-time_series_visualizer.draw_line_plot()
-time_series_visualizer.draw_bar_plot()
-time_series_visualizer.draw_box_plot()
+#time_series_visualizer.draw_line_plot()
+#time_series_visualizer.draw_bar_plot()
+#time_series_visualizer.draw_box_plot()
 
 # Run unit tests automatically
-main(module='test_module', exit=False)
+#main(module='test_module', exit=False)
+
+#----my code starts here----
+import pandas as pd
+import matplotlib.pyplot as plt
+from pandas.plotting import register_matplotlib_converters
+register_matplotlib_converters()
+
+df = pd.read_csv("fcc-forum-pageviews.csv").set_index("date")
+df = df[(df["value"]<= df["value"].quantile(0.975))]
+
+fig, ax = plt.subplots(figsize=(20,10)) 
+line_plot = plt.plot(df.index, df["value"])
+plt.xlabel("Date")
+plt.ylabel("Page Views")
+plt.title("Daily freeCodeCamp Forum Page Views 5/2016-12/2019")
+fig.savefig('line_plot.png')
+
+#print(plt.show)
--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -5,14 +5,15 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = None
+df = pd.read_csv("fcc-forum-pageviews.csv").set_index("date")
 
 # Clean data
-df = None
+df = df[(df["value"]<= df["value"].quantile(0.975))]
 
 
 def draw_line_plot():
     # Draw line plot
+    
 
 
 
